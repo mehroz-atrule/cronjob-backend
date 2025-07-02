22e5dc38aad5e9cb4338a2a1e34f43fb5e459a13
Mehroz farooq <mehroz.farooq@atrule.com>
2025-07-02 13:30:08 +0500

    x

--- api/cron/route.js
+++ api/cron/route.js
@@ -1,10 +1,18 @@
 // app/api/cron/route.js
 
 import { NextResponse } from "next/server";
+import { runCronJob } from "../utils/runCronJob";
 
 export async function GET() {
-  // Your scheduled task logic goes here
-  console.log("Cron job executed!");
+  try {
+    // Your scraping + DB logic here
+    await runCronJob();
+    console.log("Cron job executed!");
 
-  return NextResponse.json({ ok: true });
+    return NextResponse.json({ ok: true });
+  } catch (err) {
+    console.error("Error executing cron job:", err.message);
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+  // Your scheduled task logic goes here
 }
